dde218bae37863e2325916e9cefc1cab89866a8c
Cian O'Gralaigh <ihatecian@gmail.com>
2020-11-01 15:05:14 +0000

    Add typed cntext

---
+++ src/Components/Containers/CounterContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {}
+
+const CounterContainer: React.FC<Props> = () => {
+  return <div>CounterContainer </div>;
+};
+
+export default CounterContainer;
---
+++ src/Context/CounterContext.tsx
@@ -0,0 +1,58 @@
+import React, {
+  createContext,
+  Dispatch,
+  ReactElement,
+  useReducer,
+} from 'react';
+
+interface CounterState {
+  count: number;
+}
+
+enum ACTION_TYPES {
+  INC_COUNTER = 'INC_COUNTER',
+  DEC_COUNTER = 'DEC_COUNTER',
+}
+
+interface Action {
+  type: ACTION_TYPES;
+  payload: number;
+}
+
+const initialState: CounterState = { count: 0 };
+
+const ContainerContext = createContext<{
+  state: typeof initialState;
+  dispatch: (action: Action) => void;
+}>({
+  state: initialState,
+  dispatch: () => {},
+});
+
+const counterReducer: React.Reducer<CounterState, Action> = (
+  state: CounterState = initialState,
+  action: Action
+) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+interface Props {
+  children: ReactElement;
+}
+
+const CounterContextProvider: React.FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(counterReducer, initialState);
+
+  const value = { state, dispatch };
+
+  return (
+    <ContainerContext.Provider value={value}>
+      {children}
+    </ContainerContext.Provider>
+  );
+};
+
+export default CounterContextProvider;
